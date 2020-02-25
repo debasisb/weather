7bc55746b9f27e6b9220ec1a07e83aa9516398b6
saysha9 <srj@hawaii.edu>
2020-02-25 22:19:33 +0000

    changed api key

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.post("/", function(req, res) {
     
     //build up the URL for the JSON query, API Key is // secret and needs to be obtained by signup 
         const units = "imperial";
-        const apiKey = "67f6b382921c1e89b39b20d4f9556f22";
+        const apiKey = "29287f9bdce281a4c0ea5ddf3e3feff3";
         const url = "https://api.openweathermap.org/data/2.5/weather?zip=" + zip +  "&units=" + units + "&APPID=" + apiKey;
     
     // this gets the data from Open WeatherPI
