10ac4513d138f943eae493db9fc27cd61b13245d
Debasis Bhattacharya <debasisb@hawaii.edu>
2020-02-25 09:27:01 +0000

    Commented out the local http server code. Dont need that in Repl but need it while running in localhost:3000

--- index.js
+++ index.js
@@ -45,6 +45,9 @@ app.post("/", function(req, res) {
     });
 })
 
-app.listen(3000, function() {
-  console.log ("Server is running on port 3000")
-});
+
+//Commented out these lines in Repl
+//Uncomment these lines when running on laptop
+//app.listen(3000, function() {
+//console.log ("Server is running on port //3000")
+//});
