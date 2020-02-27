760b01590a806f996527dc45e839ad762247b1fd
Kealii Ballao <ballao@hawaii.edu>
2020-02-27 13:53:11 -1000

    Update index.js

--- index.js
+++ index.js
@@ -13,20 +13,20 @@ app.get("/", function(req, res) {
 
 //invoked after hitting go in the html form
 app.post("/", function(req, res) {
-    
+
     // takes in the zip from the html form, display in // console. Takes in as string, ex. for zip 02139
         var zip = String(req.body.zipInput);;
         console.log(req.body.zipInput);
-    
-    //build up the URL for the JSON query, API Key is // secret and needs to be obtained by signup 
+
+    //build up the URL for the JSON query, API Key is // secret and needs to be obtained by signup
         const units = "imperial";
-        const apiKey = "67f6b382921c1e89b39b20d4f9556f22";
+        const apiKey = "e2a23207324a3ca9e15f4b78edef5b69";
         const url = "https://api.openweathermap.org/data/2.5/weather?zip=" + zip +  "&units=" + units + "&APPID=" + apiKey;
-    
+
     // this gets the data from Open WeatherPI
     https.get(url, function(response){
         console.log(response.statusCode);
-        
+
         // gets individual items from Open Weather API
         response.on("data", function(data){
             const weatherData = JSON.parse(data);
@@ -35,7 +35,7 @@ app.post("/", function(req, res) {
             const weatherDescription = weatherData.weather[0].description;
             const icon = weatherData.weather[0].icon;
             const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-            
+
             // displays the output of the results
             res.write("<h1> The weather is " + weatherDescription + "<h1>");
             res.write("<h2>The Temperature in " + city + " " + zip + " is " + temp + " Degrees Fahrenheit<h2>");
