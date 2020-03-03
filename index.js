b36212479486c12feb2b4af3be7aa7c4f4c3f5b8
Kealii Ballao <ballao@hawaii.edu>
2020-03-02 17:38:59 -1000

    Updated javascript

--- index.js
+++ index.js
@@ -21,16 +21,16 @@ app.post("/", function(req, res) {
     //build up the URL for the JSON query, API Key is // secret and needs to be obtained by signup
         const units = "imperial";
         const apiKey = "e2a23207324a3ca9e15f4b78edef5b69";//secret key created on openweathermap
-        const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city +  "&units=" + units + "&APPID=" + apiKey; //url needed to query by city. See openweathermap city section
+        const url = "https://api.openweathermap.org/data/2.5/weather" + "?q=" + city +  "&units=" + units + "&APPID=" + apiKey; //url needed to query by city. See openweathermap city section
 
     // this gets the data from Open WeatherPI
     https.get(url, function(response){
         console.log(response.statusCode);
-
         // gets individual items from Open Weather API
         response.on("data", function(data){
             const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
+            const feelsLike = weatherData.main.feels_like;
             const city = weatherData.name;
             const weatherDescription = weatherData.weather[0].description;
             const icon = weatherData.weather[0].icon;
@@ -40,6 +40,9 @@ app.post("/", function(req, res) {
             res.write("<h1> The weather is " + weatherDescription + "<h1>");
             res.write("<h2>The Temperature in " + city + " is " + temp + " Degrees Fahrenheit<h2>");//took out zip variable because not needed.
             res.write("<img src=" + imageURL +">");
+            res.write("Feels like: "+ feelsLike);
+
+        
             res.send();
         });
     });
