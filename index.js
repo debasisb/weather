ef3a30714b46877a44fa9f8f495b45110cbdeaca
Kealii Ballao <ballao@hawaii.edu>
2020-03-02 17:06:44 -1000

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post("/", function(req, res) {
 
             // displays the output of the results
             res.write("<h1> The weather is " + weatherDescription + "<h1>");
-            res.write("<h2>The Temperature in " + city + " is " + temp + " Degrees Fahrenheit<h2>");
+            res.write("<h2>The Temperature in " + city + " is " + temp + " Degrees Fahrenheit<h2>");//took out zip variable because not needed.
             res.write("<img src=" + imageURL +">");
             res.send();
         });
