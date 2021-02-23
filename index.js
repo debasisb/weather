b040d3da8a445ddcb51a6b328714e683022c5efc
marcvela <marcvela@hawaii.edu>
2021-02-23 23:14:26 +0000

    updated zip to city and added two weather descriptors

--- index.js
+++ index.js
@@ -17,11 +17,13 @@ app.post("/", function(req, res) {
     // takes in the zip from the html form, display in // console. Takes in as string, ex. for zip 02139
         var zip = String(req.body.zipInput);
         console.log(req.body.zipInput);
+        var city = String(req.body.cityInput);
+        console.log(req.body.cityInput)
     
     //build up the URL for the JSON query, API Key is // secret and needs to be obtained by signup 
         const units = "imperial";
-        const apiKey = "67f6b382921c1e89b39b20d4f9556f22";
-        const url = "https://api.openweathermap.org/data/2.5/weather?zip=" + zip +  "&units=" + units + "&APPID=" + apiKey;
+        const apiKey = "2772dd5b7e80a5b58de7f2713214959c";
+        const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city +  "&units=" + units + "&APPID=" + apiKey;
     
     // this gets the data from Open WeatherPI
     https.get(url, function(response){
@@ -32,13 +34,15 @@ app.post("/", function(req, res) {
             const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
             const city = weatherData.name;
+            const wind = weatherData.wind.speed;
+            const humidity = weatherData.main.humidity;
             const weatherDescription = weatherData.weather[0].description;
             const icon = weatherData.weather[0].icon;
             const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
             
             // displays the output of the results
             res.write("<h1> The weather is " + weatherDescription + "<h1>");
-            res.write("<h2>The Temperature in " + city + " " + zip + " is " + temp + " Degrees Fahrenheit<h2>");
+            res.write("<h2>The Temperature in " + city + " " + " is " + temp + " Degrees Fahrenheit and the wind speed is " + wind + "MPH with a current humidity of " + humidity + "%. <h2>");
             res.write("<img src=" + imageURL +">");
             res.send();
         });
