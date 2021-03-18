aaa5d324b820c2f6fca0c0ca804500d9c511f43c
Debasis Bhattacharya <debasisb@hawaii.edu>
2021-03-18 01:48:31 +0000

    Query weather with city code

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ app.get("/", function(req, res) {
 app.post("/", function(req, res) {
     
     // takes in the zip from the html form, display in // console. Takes in as string, ex. for zip 02139
-        var zip = String(req.body.zipInput);
-        console.log(req.body.zipInput);
+        var cityid = String(req.body.cityid);
+        console.log(req.body.cityid);
     
     //build up the URL for the JSON query, API Key is // secret and needs to be obtained by signup 
         const units = "imperial";
         const apiKey = "67f6b382921c1e89b39b20d4f9556f22";
-        const url = "https://api.openweathermap.org/data/2.5/weather?zip=" + zip +  "&units=" + units + "&APPID=" + apiKey;
+        const url = "https://api.openweathermap.org/data/2.5/weather?id=" + cityid +  "&units=" + units + "&APPID=" + apiKey;
     
     // this gets the data from Open WeatherPI
     https.get(url, function(response){
@@ -30,16 +30,18 @@ app.post("/", function(req, res) {
         // gets individual items from Open Weather API
         response.on("data", function(data){
             const weatherData = JSON.parse(data);
-            const temp = weatherData.main.temp;
             const city = weatherData.name;
-            const weatherDescription = weatherData.weather[0].description;
-            const icon = weatherData.weather[0].icon;
-            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            const maxtemp = weatherData.main.temp;
+            const windspeed = weatherData.wind.speed;
+            const winddeg = weatherData.wind.deg;
+            const clouds = weatherData.clouds.all;
             
             // displays the output of the results
-            res.write("<h1> The weather is " + weatherDescription + "<h1>");
-            res.write("<h2>The Temperature in " + city + " " + zip + " is " + temp + " Degrees Fahrenheit<h2>");
-            res.write("<img src=" + imageURL +">");
+            res.write("<h1> The weather for city " + city + "<h1>");
+            res.write("<h2>The Temperature in " + city + " is " + maxtemp + " Degrees Fahrenheit<h2>");
+            res.write("<h2>The Wind Speed and Direction in " + city + " is " + windspeed + " mph<h2>");
+            res.write("<h2>The Wind Direction in " + city + " is " + winddeg + " degrees<h2>");
+            res.write("<h2>The Cloudiness in " + city + " is " + clouds + " % <h2>");
             res.send();
         });
     });
