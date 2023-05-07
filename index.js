5c65429e03c2ff7d229ad28939c9cb44a7dff880
btuehara <122405572+btuehara@users.noreply.github.com>
2023-05-07 10:58:04 -1000

    Update index.js

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ app.get("/", function(req, res) {
 app.post("/", function(req, res) {
     
     // takes in the zip from the html form, display in // console. Takes in as string, ex. for zip 02139
-        var zip = String(req.body.zipInput);
-        console.log(req.body.zipInput);
+        var cityID = String(req.body.idInput);
+        console.log(req.body.idInput);
     
     //build up the URL for the JSON query, API Key is // secret and needs to be obtained by signup 
         const units = "imperial";
         const apiKey = "67f6b382921c1e89b39b20d4f9556f22";
-        const url = "https://api.openweathermap.org/data/2.5/weather?zip=" + zip +  "&units=" + units + "&APPID=" + apiKey;
+        const url = "https://api.openweathermap.org/data/2.5/weather?id=" + cityID +  "&units=" + units + "&APPID=" + apiKey;
     
     // this gets the data from Open WeatherPI
     https.get(url, function(response){
@@ -34,11 +34,16 @@ app.post("/", function(req, res) {
             const city = weatherData.name;
             const weatherDescription = weatherData.weather[0].description;
             const icon = weatherData.weather[0].icon;
+            const speed = weatherData.wind.speed;
+            const direction = weatherData.wind.deg;
+            const cloudiness = weatherData.clouds.all;
+            const humidity = weatherData.main.humidity;
             const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
             
             // displays the output of the results
             res.write("<h1> The weather is " + weatherDescription + "<h1>");
-            res.write("<h2>The Temperature in " + city + " " + zip + " is " + temp + " Degrees Fahrenheit<h2>");
+            res.write("<h2>The Temperature in " + city + " is " + temp + " Degrees Fahrenheit<h2>");
+            res.write("<h2>The windspeed is " + speed + "mph, " + " and the wind direction is " + direction + " degrees" + ", and the cloudiness is " + cloudiness + "%" + " and the humidity is " + humidity + " %<h2>");
             res.write("<img src=" + imageURL +">");
             res.send();
         });
@@ -50,3 +55,4 @@ app.post("/", function(req, res) {
 app.listen(process.env.PORT || 3000, function() {
 console.log ("Server is running on port")
 });
+
